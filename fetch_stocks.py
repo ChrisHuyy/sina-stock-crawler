ac3fa0e5445c6b2aff8eed9c4cd262be95dbbe16
Chris Hu <33180412+ChrisHuyy@users.noreply.github.com>
2018-05-03 21:29:12 +0800

    Add files via upload

--- fetch_stocks.py
+++ fetch_stocks.py
@@ -1,6 +1,11 @@
 # coding=utf-8
 import requests
 import demjson
+import datetime
+import os
+import time
+from dao import query,execute,q
+
 
 stock_url = "http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData"\
 			"?page={page}&num={num}&sort=symbol&asc=1&node={node}&symbol=&_s_r_a=init"
@@ -13,6 +18,7 @@ sz_a_mkt = {'page':1,'num' :40,'node':"sz_a"}
 # 深市B股
 sz_a_mkt = {'page':1,'num' :40,'node':"sz_b"}
 
+
 def req(url):
 	try:
 		r = requests.get(url,timeout=10)
@@ -24,6 +30,7 @@ def req(url):
 		return r.content
 	else:
 		return
+
 
 def get_stocks_package(stock_url,mkt,stocks):
 	package = req(stock_url.format(**mkt))
@@ -39,6 +46,54 @@ def get_stocks_package(stock_url,mkt,stocks):
 			return get_stocks_package(stock_url,mkt,stocks)
 
 
-if __name__ == '__main__':
+def store_as_file(infoObj):
+	file_name = os.path.dirname(os.path.realpath(__file__)) + "\save\s_%s.log" % datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
+	with open(file_name,'w') as f:
+		f.write(demjson.encode(infoObj))
+	print "Data has been store at %s" % file_name
+
+
+def store_in_db(infoObj):
+	for info in infoObj:
+		info['ctime'] = int(time.time())
+		info['last_update_time'] = int(time.time())
+		if check_redandunt(info):
+			update(info,pk='symbol')
+		else:
+			insert(info)
+
+def check_redandunt(info):
+	if info.has_key('symbol'):
+		query_sql = "SELECT * FROM `stocks_info` WHERE `symbol` = '{}'".format(info['symbol'])
+		ret = query(query_sql)
+		if ret is not None and len(ret) > 0:
+			return True
+		else:
+			return False
+
+def update(info,pk):
+	update_list = []
+	for k in info.keys():
+		update_part = "`{}`='{}'".format(k,q(info[k]))
+		update_list.append(update_part)
+	update_sql = "UPDATE `stocks_info` SET {} WHERE `{}` = '{}'".format(",".join(update_list),pk,info[pk])
+	execute(update_sql)
+
+
+def insert(info):
+	k_list = []
+	v_list = []
+	for k in info.keys():
+		k_list.append(k)
+		v_list.append(q(info[k]))
+	insert_sql = "INSERT INTO `stocks_info`(`{}`)VALUES('{}')".format("`,`".join(k_list),"','".join(v_list))
+	execute(insert_sql)
+
+
+def main():
 	stocks_sh_a = get_stocks_package(stock_url,sh_a_mkt,stocks=[])
-	print stocks_sh_a
+	store_in_db(stocks_sh_a)
+	store_as_file(stocks_sh_a)	
+
+if __name__ == '__main__':
+	main()
